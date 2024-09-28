97ca2a1200e0f9b56246d1d18b715ac328714abc
Brayan Figueroa <brayanfigueroajerez@gmail.com>
2024-09-28 14:21:49 -0500

    update for netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../src/pages/Home';
-
+import NavBar from './components/NavBar';
+import AppRoutes from './routes/AppRoutes';
 import "./index.css"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <main>
+        <AppRoutes />
+      </main>
+    </div>
   );
 };
 
