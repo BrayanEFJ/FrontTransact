ffa1ec534d8d8ba6381999fec11f7619c576e063
Brayan Figueroa <brayanfigueroajerez@gmail.com>
2024-10-03 13:49:08 -0500

    update for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import AppRoutes from './routes/AppRoutes';
-import "./index.css"
-import "./i18n.js";
+import React from "react";
+import "./index.css";
+import Home from "./pages/Home";
+import "./i18n.js"
 
 const App = () => {
   return (
-    <div className="App">
-      <main>
-        <AppRoutes />
-      </main>
-    </div>
+    <>
+      <Home />
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 
+const router = createBrowserRouter([
+  {
+    path: "/TransactFront/",
+    element: <App />,
+    children:[
+      {
+        path: "/TransactFront/",
+        element: <Home/>
+      }
+    ] 
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <RouterProvider router={router} />
 );
